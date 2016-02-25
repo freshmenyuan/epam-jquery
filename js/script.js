5d11b840619dec1954d1d192b978fcfe8ebe0c59
freshmanyuan <freshmanyuan@163.com>
2016-02-25 15:18:25 +0800

    animation answers

--- js/script.js
+++ js/script.js
@@ -45,6 +45,38 @@ $("#form2 button").click(function() {
 
 // handle the mouseover event here
 
+$("#animate1 .img-responsive").mouseover(function (){
+    $(".img-responsive").attr("style","width:70%;");
+});
+
+$("#animate1 .img-responsive").mouseleave(function (){
+    $(".img-responsive").attr("style","width:20%;");
+});
+
+
+
+$("#animate2 .img-responsive").click(function(){
+    console.log("click image2");
+    $(this).css("position","relative");
+   $(this).animate({
+      "left":"80%"
+    },500);
+});
+
+
+$("#animate3 .img-responsive").click(function(){
+        console.log("click image3");
+    $(this).animate({
+      "opacity": "0.4"
+    },600, function(){
+        $(this).attr("src","images/cat4.jpg");
+    });
+    
+    $(this).animate({
+      "opacity": "1"
+    },600);
+});
+
 // handle the form events here
 
 $("#formEvents form").submit(function(event) {
